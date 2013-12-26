69098004f602f89e37a9967636f99659725f3738
Lindsey Dew <lindsey.dew@guardian.co.uk>
2013-12-26 17:18:59 +0000

    make the deployment script better

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,9 @@
 #bin/bash
-cd /var/play
-if [ -d blog.orig ]; then
-    rm -rf blog.orig
-fi
-mv blog blog.orig
-git clone ~/git/blog.git
-cd blog
+cd /var/play/blog
+git fetch
+git merge origin/master
 /var/play/play-2.1.0/play clean compile stage
 chmod +x target/start
 sudo service lindseysblog restart
-rm -rf blog.orig
 
 
