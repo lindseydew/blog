76e01a9098cec75682bd7783ed5317a8c0b78363
Lindsey Dew <lindsey.dew@guardian.co.uk>
2013-12-26 16:48:55 +0000

    add a deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#bin/bash
+cd /var/play
+if [ -d blog.orig ]; then
+    rm -rf blog.orig
+fi
+mv blog blog.orig
+git clone ~/git/blog.git
+cd blog
+/var/play/play-2.1.0/play clean compile stage
+chmod +x target/start
+sudo service lindseysblog restart
+rm -rf blog.orig
